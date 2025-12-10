36b8da66d37e2b0d1b0b6fe20ed411f403b31ca9
Santiago_sana07 <santiago.sanabria27599@ucaldas.edu.co>
2025-12-10 17:47:02 -0500

    v0.0.3

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,33 +1,27 @@
-import dotenv from "dotenv";
-dotenv.config();
+import dotenv from "dotenv"
+dotenv.config()
 
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
+import { Sequelize } from "sequelize"
 
-import { Sequelize } from "sequelize";
-
-if (!process.env.DB_HOST) {
-  throw new Error("DATABASE_URL is not defined");
-}
-
-export const sequelize = new Sequelize(process.env.DB_HOST, {
-  dialect: "postgres",
-  protocol: "postgres",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // obligatorio para Supabase
-    },
-  },
-});
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    logging: false
+  }
+)
 
 async function dbConnect() {
   try {
-    await sequelize.authenticate();
-    console.log("DB Connected");
+    await sequelize.authenticate()
+    console.log("DB Connected")
   } catch (error) {
-    console.error("DB Error", error);
+    console.error("DB Error", error)
   }
 }
 
-dbConnect();
+dbConnect()
